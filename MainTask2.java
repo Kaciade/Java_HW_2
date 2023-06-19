8c1d6a61351aa71ff757469ec60a93ef8c1df878
Kaciade <valerypolyakov60@gmail.com>
2023-06-19 15:25:42 +0200

    Задание 2 выполнено

---
+++ MainTask2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+ 
+public class MainTask2 {
+    public static void main(String[] args) {
+        int [] mas = {146, 813, 47, 19, 743};
+ 
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+ 
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
